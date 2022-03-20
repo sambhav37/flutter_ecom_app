1381a31cd9623f6bf1876bb1ec73154ded3e59cd
kusambhav <kusambhav@expediagroup.com>
2022-03-20 17:17:56 +0530

    Adding filter item capability

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,7 +2,19 @@ import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favourites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+
+  var _showOnlyFavourites = false;
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +23,34 @@ class ProductsOverviewScreen extends StatelessWidget {
         title: Text(
           'MyShop',
         ),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favourites) {
+                _showOnlyFavourites = true;
+              } else {
+                _showOnlyFavourites = false;
+              }
+              });
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favourites'),
+                value: FilterOptions.Favourites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavourites),
     );
   }
 }
-
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -8,12 +8,16 @@ import '../providers/products.dart';
 
 class ProductsGrid extends StatelessWidget {
 
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
+
   @override
   Widget build(BuildContext context) {
     // we want to set connection to the provider that provides intsnace of Product class
     // this like will look at parent Products grid, no product, then the above parent
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products = showFavs ? productsData.favouriteItems : productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
