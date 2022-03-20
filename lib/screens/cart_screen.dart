6d43364999e06e2ce5b36c179ba4af9990f17af7
kusambhav <kusambhav@expediagroup.com>
2022-03-20 20:40:04 +0530

    Added stateful widget and orders page

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -29,7 +29,7 @@ class CartScreen extends StatelessWidget {
                   ),
                   Spacer(),
                   Chip(
-                    label: Text('\$${cart.totalAmount}',
+                    label: Text('\$${cart.totalAmount.toStringAsFixed(2)}',
                         style: TextStyle(
                           color: Theme.of(context)
                               .primaryTextTheme
