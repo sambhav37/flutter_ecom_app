f0f1cca68a8cfbb88027f6b4c051b46cbada93f0
kusambhav <kusambhav@expediagroup.com>
2022-03-20 21:22:23 +0530

    Adding Snackbar

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -27,7 +27,7 @@ class Cart with ChangeNotifier {
 
   double get totalAmount {
     var total = 0.0;
-    _items.forEach((key, cartItem) { 
+    _items.forEach((key, cartItem) {
       total += cartItem.price * cartItem.quantity;
     });
     return total;
@@ -70,6 +70,25 @@ class Cart with ChangeNotifier {
 
   void clear() {
     _items = {};
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          quantity: existingCartItem.quantity - 1,
+          price: existingCartItem.price,
+        ));
+    } else {
+      _items.remove(productId);
+    }
     notifyListeners();
   }
 }
